afe784afc0ab1821760f79ef5d531ea837406b01
Danan Purwantoro <danan.purwantoro@kompas.com>
2024-04-22 10:55:17 +0700

    remove __main__ for simplicity

--- kalkulator-listrik.py
+++ kalkulator-listrik.py
@@ -12,35 +12,30 @@ def calculate_tarif_listrik(devices, operation_duration, tarif_dasar):
     return tarif_listrik
 
 
-def main():
-    st.title("Kalkulator tarif listrik")
+st.title("Kalkulator tarif listrik")
 
-    # Input fields for devices and their power ratings
-    number_of_devices = 3
-    devices = {}
-    operation_duration = {}
-    for i in range(number_of_devices):
-        device_name = f"Device {i+1}"
-        power_rating = st.number_input(
-            f"Daya listrik {device_name} (watt)", min_value=0, key=device_name)
-        devices[device_name] = power_rating
-        duration = st.number_input(
-            f"Durasi {device_name} (jam)", min_value=0, key=f"{device_name}-duration")
-        operation_duration[device_name] = duration
+# Input fields for devices and their power ratings
+number_of_devices = 3
+devices = {}
+operation_duration = {}
+for i in range(number_of_devices):
+    device_name = f"Device {i+1}"
+    power_rating = st.number_input(
+        f"Daya listrik {device_name} (watt)", min_value=0, key=device_name)
+    devices[device_name] = power_rating
+    duration = st.number_input(
+        f"Durasi {device_name} (jam)", min_value=0, key=f"{device_name}-duration")
+    operation_duration[device_name] = duration
 
-    tarif_dasars = {"R-1/TR daya 900 VA": 1352,
-                    "R-1/TR daya 1.300 VA": 1444.70, "R-1/TR daya 2.200 VA": 1444.70, "R-2/TR daya 3.500-5.500 VA": 1699.53}
+tarif_dasars = {"R-1/TR daya 900 VA": 1352,
+                "R-1/TR daya 1.300 VA": 1444.70, "R-1/TR daya 2.200 VA": 1444.70, "R-2/TR daya 3.500-5.500 VA": 1699.53}
 
-    # Selectbox to choose tarif dasar
-    selected_tarif_dasar = st.selectbox(
-        "Piih tarif dasar", list(tarif_dasars.keys()))
+# Selectbox to choose tarif dasar
+selected_tarif_dasar = st.selectbox(
+    "Piih tarif dasar", list(tarif_dasars.keys()))
 
-    # Calculate total power consumed
-    if st.button("Calculate"):
-        tarif_litrik = calculate_tarif_listrik(
-            devices, operation_duration, tarif_dasars[selected_tarif_dasar])
-        st.write(f"Tarif listrik: Rp {tarif_litrik}")
-
-
-if __name__ == "__main__":
-    main()
+# Calculate total power consumed
+if st.button("Calculate"):
+    tarif_litrik = calculate_tarif_listrik(
+        devices, operation_duration, tarif_dasars[selected_tarif_dasar])
+    st.write(f"Tarif listrik: Rp {tarif_litrik}")
