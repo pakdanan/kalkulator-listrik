cc47b79c9594c98eddd239b34eddaae2cb3e7cc1
Danan Purwantoro <danan.purwantoro@kompas.com>
2024-04-22 09:03:15 +0700

    simplify

--- kalkulator-listrik.py
+++ kalkulator-listrik.py
@@ -14,22 +14,17 @@ def main():
     st.title("Total Power Consumed Calculator")
 
     # Input fields for devices and their power ratings
-    st.header("Devices and Power Ratings")
     devices = {}
-    for i in range(1, 5):
-        device_name = st.text_input(f"Device {i} name")
+    operation_duration = {}
+    for i in range(4):
+        device_name = st.text_input(f"Device {i+1} name")
         power_rating = st.number_input(
-            f"Power rating of {device_name} (in watts)", min_value=0, key=f"device-{i}")
+            f"Power rating of {device_name} (in watts)", min_value=0)
         if device_name:
             devices[device_name] = power_rating
-
-    # Input fields for operation durations
-    st.header("Operation Durations")
-    operation_duration = {}
-    for device in devices.keys():
-        duration = st.number_input(
-            f"Operation duration of {device} (in hours)", min_value=0, key=f"device-duration-{i}")
-        operation_duration[device] = duration
+            duration = st.number_input(f"Operation duration of {
+                                       device_name} (in hours)", min_value=0)
+            operation_duration[device_name] = duration
 
     # Calculate total power consumed
     if st.button("Calculate"):
